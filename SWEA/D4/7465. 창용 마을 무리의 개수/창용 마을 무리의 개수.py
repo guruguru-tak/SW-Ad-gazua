0d59d43019927957e84372e974755f83efb06472
guruguru-tak <kkt3289@gmail.com>
2024-09-02 17:12:01 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 198 ms, Memory: 51,440 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.py
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.py
@@ -2,20 +2,28 @@ from collections import deque
 t = int(input())
 
 
-def bfs(i, adj_list, visited, n_top):
+def bfs(i, adj_list, visited):
     q = deque()
+    #넘어온 i 인덱스 값을 먼저 확인 후 방문처리
     q.append(i)
-    visited[i] = True
+    visited[i] is True
 
+    #큐가 빌 때까지 반복
     while q:
+        #현재 넣은 값 v로 꺼내서
         v = q.popleft()
+        # v 번 인덱스 해당하는 인접리스트 안에 값 전부 참조
         for g in range(len(adj_list[v])):
-            if not visited[adj_list[v][g]]:
+            #방문처리 안되어 있으면
+            if visited[adj_list[v][g]] is False:
+                #방문처리하고
                 visited[adj_list[v][g]] = True
+                #큐에 넣기
                 q.append(adj_list[v][g])
 
 for tc in range(1, t + 1):
     n_top, m_node = map(int, input().split())
+
 
     adj_list = [[] for _ in range(n_top+1)]
     visited = [False] * (n_top+1)
@@ -23,17 +31,23 @@ for tc in range(1, t + 1):
 
     #그래프 입력 구성
     for _ in range(m_node):
-        x, y = map(int, input().split())
-        #방향 그래프일 경우 한 줄만
-        adj_list[x].append(y)
-        #무방향이라면 양 방향도 들어와야 한다
-        adj_list[y].append(x)
+        line = list(map(int, input().split()))
+        # 연결 없이 관계없는 정점 제거
+        if len(line) > 1:
+            v, g = line
+            #방향 그래프일 경우 한 줄만
+            adj_list[v].append(g)
+            #무방향이라면 양 방향도 들어와야 한다
+            adj_list[g].append(v)
 
     result = 0
     for i in range(1, n_top+1):
-        if not visited[i]:
+        #방문하지 않으면
+        if visited[i] is False:
+            #무리 갯수 추가 후
             result += 1
-            bfs(i, adj_list, visited, n_top)
+            #bfs 호출
+            bfs(i, adj_list, visited)
 
 
     print(f"#{tc} {result}")
