a09027505566aa6b1280409db3e49a37be7c3b5e
guruguru-tak <kkt3289@gmail.com>
2024-09-09 17:24:29 +0900

    [D4] Title: 정사각형 방, Time: 2,304 ms, Memory: 83,040 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.py
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.py
@@ -1,38 +1,40 @@
+
 t = int(input())
 
+dx = [1, 0, 0, -1]
+dy = [0, 1, -1, 0]
 
 def dfs(y, x):
-    global cnt
-    dx = [1, 0, 0, -1]
-    dy = [0, 1, -1, 0]
+    #종료 조건?
 
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 > ny or ny >= n or 0 > nx or nx >= n: continue
-        #정확하게 들어온 y,x 의 값에서 정확히 1 더한게 next값고 같으면 cnt + 1,
-        if (grid[y][x] + 1) == grid[ny][nx]:
-            cnt += 1
-            #넥스트 좌표에 재귀
-            dfs(ny, nx)
+    global cnt
+
+    for dist in range(4):
+        ny, nx = y + dy[dist], x + dx[dist]
+        if 0 <= ny < n and 0 <= nx < n:
+            if(grid[y][x] + 1) == grid[ny][nx]:
+                cnt += 1
+                dfs(ny, nx)
 
 
 for tc in range(1, t+1):
     n = int(input())
+
     grid = [list(map(int, input().split())) for _ in range(n)]
-
     max_count = 0
-    start_index = 0
+    start_idx = 0
 
+
+    #모든 y, x 탐색 필요
     for y in range(n):
         for x in range(n):
-            #한 칸을 띄운 후, 조건 있어 매 번 1로 초기화
+            # 자기 자신을 포함해서 1 시작
             cnt = 1
             dfs(y, x)
 
-            if cnt > max_count or (cnt == max_count and grid[y][x] < start_index):
-                #최대 값을 max함수로 구하는게 아니라 그냥 가장 큰 것이면 값을 그대로 넣어 갱신
-                start_index = grid[y][x]
+            if cnt > max_count or (cnt == max_count and grid[y][x] < start_idx):
+                start_idx = grid[y][x]
                 max_count = cnt
+    print(f"#{tc} {start_idx} {max_count}")
 
-    print(f"#{tc} {start_index} {max_count}")
+
