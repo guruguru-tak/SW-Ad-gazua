378218bd45031a0983531825496b8d6c630e98cc
guruguru-tak <kkt3289@gmail.com>
2024-09-12 10:35:26 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문2, Time: 120 ms, Memory: 46,080 KB -BaekjoonHub

---
+++ SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.py
@@ -0,0 +1,30 @@
+t = 10
+for tc in range(1, t+1):
+    onebon_n = int(input())
+    onebon_pwd = list(map(int, input().split()))
+    cmd_n = int(input())
+    cmd = list(input().split())
+
+    for c in range(len(cmd)):
+
+        if cmd[c] == 'I':
+            idx = int(cmd[c+1])
+            # print(tc, "idx", idx)
+            num_cnt = int(cmd[c+2])
+            # print(tc, "num_cnt", num_cnt)
+            for n in range(c+3, c+3+num_cnt):
+                onebon_pwd.insert(idx, int(cmd[n]))
+                #추가하면서 바로 뒤에 추가 이어가기에 idx 증가해야 함
+                idx += 1
+        elif cmd[c] == 'D':
+            idx = int(cmd[c+1])
+            num_cnt = int(cmd[c+2])
+            for n in range(c+3, c+3+num_cnt):
+                onebon_pwd.pop(idx)
+                idx+1
+        else:
+            pass
+    print(f"#{tc}", end=" ")
+    for i in range(10):
+        print(onebon_pwd[i], end=" ")
+    print()
