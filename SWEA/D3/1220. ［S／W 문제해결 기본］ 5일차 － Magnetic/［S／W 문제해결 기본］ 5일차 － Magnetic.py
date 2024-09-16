131ed11dc3a7b643c7c811ec72a8c93339c70e06
guruguru-tak <kkt3289@gmail.com>
2024-09-16 18:49:47 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 213 ms, Memory: 50,308 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
@@ -3,35 +3,25 @@ for tc in range(1, t+1):
     n = int(input())
     grid = [list(map(int, input().split())) for _ in range(n)]
 
-    #1은 N극 2는 S극
-    # 위는 N극, 밑은 S극
-    # 서로 맞다으면 자성 연결
 
-    #맞닿은 것 걧수 카운트 변수
     cnt = 0
     for x in range(n):
+        # n극 s극 만났는지 flag 매 row마다 fasle 디폴트
+        flag = False
         for y in range(n):
-            #col 만 움직임, N 극 선택이면
-            if grid[y][x] == 1:
-                #본인 위치 1 제거 위해 1부터 인덱스 시작
-                for c in range(1, n):
-                    #벽세우기
-                    if 0 <= y+c < n:
-                        #1 바로 다음 2만나면 카운트 세기
-                        #break 걸면 현재 돌고 있는 c 부분 for 문 종료
-                        #외부 y for문 작동한다
-                        #맨처음 0, 0 에서 y가 0인 경우 break
-                        if grid[y+c][x] == 2:
-                            cnt += 1
-                            break
-                        #만약 바로 다음에 0이나 1이 있으면 바로 정지
-                        elif grid[y+c][x] == 1:
-                            break
-                        #다음 칸 0이면 다음 진행 위해 pass
-                        elif grid[y+c][x] == 0:
-                            pass
-            # 0이나 2면 패스
+            # n극 이면 1이고,
+            # 1 인식했다는 flag = True 설정
+            # 1 -> 1 -> 2 라도 결국 1 -> 2 면 만난 것
+            # 2 -> 1 이 경우만 피하면 됨
+            if grid[y][x] == 1 and not flag:
+                flag = True
+            # 1 인식했고, 그 사이에 2만나면 cnt 증가
+            # 다시 flag = false
+            elif grid[y][x] == 2 and flag:
+                cnt += 1
+                flag = False
             else:
                 pass
 
     print(f"#{tc} {cnt}")
+
