dc53b4a5ad9680f1645a74cd806d77488a0644e3
guruguru-tak <kkt3289@gmail.com>
2024-09-16 17:27:33 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 108 ms, Memory: 43,464 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
@@ -1,16 +1,19 @@
+
 t = 10
-def powpow(n, exp):
-    
-    #종료 조건 exp가 1이면 2의 1승인 자기 자신 리턴
+
+def powpow(exp):
+    # print(exp)
+    #종료 조건
     if exp == 1:
+        # 반환할 값 n -> 처음 들어온 exp 까지 리턴 반복
         return n
-    # exp -1 -> 하면 자기자신을 호출할 수록 8 7 6 5 4 로 줄어든다
-    # for 문 쓸 필요 없음
-    return n * powpow(n, exp-1)
+    # n 에 반환받는 재귀함수 선언, 파라미터는 1씩 감소
+    # 최종 exp 가 1이 되면 n*n -> n**n * n 다시 반대로 증가
+    # 마지막 최종 n**exp 의 값이 리턴 값으로 나가게 됨
+    return n*powpow(exp-1)
 
 for tc in range(1, t+1):
-    _ = int(input())
+    _ = input()
     n, e = map(int, input().split())
-    result = powpow(n, e)
 
-    print(f"#{tc} {result}")
+    print(f"#{tc} {powpow(e)}")
