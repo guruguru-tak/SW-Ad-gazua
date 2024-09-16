35754675b69afe35f829d960bb57e5b585bb4fa0
guruguru-tak <kkt3289@gmail.com>
2024-09-16 19:11:52 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - GNS, Time: 171 ms, Memory: 53,180 KB -BaekjoonHub

--- SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
+++ SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
@@ -1,20 +1,25 @@
-gns_dic = {"ZRO":0, "ONE":1, "TWO":2, "THR":3, "FOR":4, "FIV":5, "SIX":6, "SVN":7, "EGT":8, "NIN":9}
+gns = {"ZRO":0, "ONE":1, "TWO":2, "THR":3, "FOR":4, "FIV":5, "SIX":6, "SVN":7, "EGT":8, "NIN":9}
+
 
 t = int(input())
 for tc in range(1, t+1):
-    #split 기능으로  # 특수문자 두개 나누는 처리 어려움
     _, n = input().split()
-    arr = list(input().split())
+    str_gns = list(input().split())
 
-    num_arr = []
-    for gns in arr:
-        num_arr.append(gns_dic.get(gns))
+    num = []
+    for i in str_gns:
+        # gns 딕셔너리에서 입력 값 i 의 동일 키의 밸류 값 반환
+        num.append(gns.get(i))
 
-    num_arr.sort()
+    # 오름차순 정렬
+    num.sort()
+    gns_to = []
+    for i in num:
+        # 키, 밸류 값을 gns에서 item으로 분리해서 들고와서
+        for k, v in gns.items():
+            # 만약 숫자 i 가 gns의 밸류값과 같으면
+            if i == v:
+                # 키값을 반환
+                gns_to.append(k)
 
-    result = []
-    for num in num_arr:
-        result.extend([k for k, v in gns_dic.items() if v == num])
-
-
-    print(f"#{tc}", *result)
+    print(f"#{tc} ",*gns_to)
