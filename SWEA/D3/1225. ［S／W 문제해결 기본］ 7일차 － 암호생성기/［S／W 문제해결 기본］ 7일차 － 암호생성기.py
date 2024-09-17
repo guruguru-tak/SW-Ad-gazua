c6567ac3387516027b205e2118c1bdaadb0f5b03
guruguru-tak <kkt3289@gmail.com>
2024-09-17 14:17:38 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 169 ms, Memory: 49,828 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
@@ -1,26 +1,19 @@
 from collections import deque
+
 t = 10
 for tc in range(1, t+1):
+    _ = input()
+    q = deque(map(int, input().split()))
 
-    _ = int(input())
-    # 빈큐 기본형 ([])
-    q = deque(list(map(int, input().split())))
-    # print(q[7])
+    flag = True
+    while flag:
+        for i in range(1, 6):
+            temp = q.popleft()
+            if temp - i <= 0:
+                q.append(0)
+                flag = False
+                break
+            else:
+                q.append(temp-i)
 
-    while q[7] != 0:
-        cycle = 5
-        if q[7] != 0:
-            for i in range(1, cycle+1):
-                left = q.popleft()
-                left -= i
-                #0보다 작거나 같으면
-                if left <= 0:
-                    #0을 추가
-                    q.append(0)
-                    break
-                else:
-                    q.append(left)
-        else:
-            break
-    #큐를 리스트로 바꾸고 아스트릭으로 리스트 벗겨서 공백과 함께 출력
     print(f"#{tc}", *list(q))
