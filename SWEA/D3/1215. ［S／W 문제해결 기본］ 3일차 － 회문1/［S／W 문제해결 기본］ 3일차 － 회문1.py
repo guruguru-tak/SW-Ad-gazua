6bab366de4da5a7d7757df1a009c2fde7ebae8b4
guruguru-tak <kkt3289@gmail.com>
2024-09-18 02:14:36 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 140 ms, Memory: 48,484 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
@@ -1,42 +1,52 @@
 t = 10
 for tc in range(1, t+1):
-    check_n = int(input())
+    repeat_Cnt = int(input())
     n = 8
+    grid = [list(input().strip()) for _ in range(8)]
 
-    grid = [list(map(str, input().strip())) for _ in range(n)]
 
-    str_line = []
-    #일자로만 인정
+    result = 0
+    # 로우 계산
     for y in range(n):
         for x in range(n):
-            str_row = []
-            str_col = []
-            for c in range(check_n):
-                if 0 <= x+c < n:
-                    str_row.append(grid[y][x+c])
-                    str_col.append(grid[x+c][y])
-            if len(str_row) == check_n and len(str_col) == check_n:
-                str_line.append(str_row)
-                str_line.append(str_col)
-
-    # 저장 리스트 인덱스 하나씩 회문체크
-    result = 0
-    for arr in str_line:
-        cnt = 0
-        for _ in range(check_n//2):
-            if arr.pop(0) == arr.pop(-1):
-                cnt += 1
+            # x 변경 시마다 cnt 리셋
+            cnt_Row = 0
+            # 회문 범위는 반복문의 몫만큼
+            for r in range(repeat_Cnt//2):
+                # 회문 길이 벽
+                if (x+repeat_Cnt-1)-r < n:
+                    # 회문 첫 번째 -> x + r
+                    # 회문 마지막 -> x+repeat_Cnt-1)-r
+                    # r 의 증가로 회문 좌우 인접 비교하게 됨
+                    if grid[y][x+r] == grid[y][(x+repeat_Cnt-1)-r]:
+                        cnt_Row += 2
+                    else:
+                        break
+            # 회문 길이 홀수, 짝수에 따라 수식 달리 한다
+            if repeat_Cnt % 2 == 0:
+                if cnt_Row == repeat_Cnt:
+                    result += 1
             else:
-                break
+                if cnt_Row + 1 == repeat_Cnt:
+                    result += 1
 
-        #짝수 경우의 갯수
-        if check_n % 2 == 0:
-            if cnt == check_n/2:
-                result += 1
+    # 컬럼 계산
+    for x in range(n):
+        for y in range(n):
+            # x 변경 시마다 cnt 리셋
+            cnt_Col = 0
+            for r in range(repeat_Cnt//2):
+                if (y+repeat_Cnt-1)-r < n:
+                    if grid[y+r][x] == grid[(y+repeat_Cnt-1)-r][x]:
+                        cnt_Col += 2
+                    else:
+                        break
 
-        #홀수 경우의 갯수
-        else:
-            if cnt == check_n//2:
-                result += 1
+            if repeat_Cnt % 2 == 0:
+                if cnt_Col == repeat_Cnt:
+                    result += 1
+            else:
+                if cnt_Col + 1 == repeat_Cnt:
+                    result += 1
 
     print(f"#{tc} {result}")
