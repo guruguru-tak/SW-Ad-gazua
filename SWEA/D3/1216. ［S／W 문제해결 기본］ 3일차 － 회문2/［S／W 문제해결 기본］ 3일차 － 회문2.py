89487fc7e8547809a21b63c41749feb738e659fd
guruguru-tak <kkt3289@gmail.com>
2024-09-20 23:25:19 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 1,514 ms, Memory: 54,604 KB -BaekjoonHub

--- SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
@@ -1,75 +1,62 @@
 t = 10
 for tc in range(1, t+1):
-    test = int(input())
+    # 테케 번호 무작위로 주기에 tc로 뽑으면 오류 남
+    testcase = int(input())
     n = 100
+    grid = [list(input().strip()) for _ in range(n)]
 
-    grid = [list(map(str, input().strip())) for _ in range(n)]
+    # 최대 길이 찾기
+    max_Result =0
 
-    max_cnt = 0
-
-    #홀수 일때
+    # 홀수 기준 최대길이 먼저 찾기
     for y in range(n):
         for x in range(n):
-            # 첫 번째 1길이 회문 A 한자리 계산
-            cnt = 1
-            for dist in range(1, n//2):
-                # 첫 번째 1길이 회문 0 번지 == 0 번지 이기에 1 추가
-                if 0 <= x - dist < n and 0 <= x + dist < n:
-                    if grid[y][x-dist] == grid[y][x+dist]:
-                        #양쪽 2개씩 추가 됨
-                        cnt += 2
-                    else:
-                        break
+            # 기본 자기 위치 1
+            cnt_row = 1
+            cnt_col = 1
+            # 회문 인덱스 2배씩 증가기에 전체 길이의 // 2 가 범위
+            for r in range(1, n//2):
+                # 벽세우기
+                if x - r < 0 or x + r >= n: continue
+                # 행
+                # 현재 인덱스 기준 좌, 우 계속 + 1 씩 해서 비교
+                if grid[y][x+r] == grid[y][x-r]:
+                    cnt_row += 2
                 else:
                     break
-
-            max_cnt = max(max_cnt, cnt)
-
-    for y in range(n):
-        for x in range(n):
-            # 첫 번째 1길이 회문 A 한자리 계산
-            cnt = 1
-            for dist in range(1, n//2):
-                if 0 <= x - dist < n and 0 <= x + dist < n:
-                    if grid[x-dist][y] == grid[x+dist][y]:
-                        # 양쪽 2개씩 추가 됨
-                        cnt += 2
-                    else:
-                        break
+            for r in range(1, n//2):
+                if x - r < 0 or x + r >= n: continue
+                # 열
+                if grid[x+r][y] == grid[x-r][y]:
+                    cnt_col += 2
                 else:
                     break
-            max_cnt = max(max_cnt, cnt)
+            # 최대값 비교
+            max_Result = max(max_Result, cnt_row, cnt_col)
 
-
-    # 짝수 일 때
-    for y in range(n):
-        for x in range(n):
-            #짝수는 cnt 2개 합쳐야 2부터 시작
-            cnt = 0
-            for dist in range(n // 2):
-                # 짝수는 어떻게?
-                if 0 <= x - dist < n and 0 <= x + dist + 1 < n:
-                    if grid[y][x-dist] == grid[y][x + dist + 1]:
-                        cnt += 2
+        # 짝수 기준 최대길이 찾기
+        for y in range(n):
+            for x in range(n):
+                # 기본 자기 위치 0
+                cnt_row = 0
+                cnt_col = 0
+                # 회문 인덱스 2배씩 증가기에 전체 길이의 // 2 가 범위
+                for r in range(1, n // 2):
+                    if x - r + 1< 0 or x + r >= n: continue
+                    # 행
+                    # 현재 인덱스와 1칸 바로 옆에 2개씩 비교
+                    if grid[y][x + r] == grid[y][x - r + 1]:
+                        cnt_row += 2
                     else:
                         break
-                else:
-                    break
-
-            max_cnt = max(max_cnt, cnt)
-
-    for y in range(n):
-        for x in range(n):
-            #짝수는 cnt 2개 합쳐야 2부터 시작
-            cnt = 0
-            for dist in range(n // 2):
-                if 0 <= x - dist < n and 0 <= x + dist + 1 < n:
-                    if grid[x - dist][y] == grid[x + dist + 1][y]:
-                        cnt += 2
+                for r in range(1, n // 2):
+                    if x - r  + 1< 0 or x + r >= n: continue
+                    # 열
+                    if grid[x + r][y] == grid[x - r + 1][y]:
+                        cnt_col += 2
                     else:
                         break
-                else:
-                    break
-            max_cnt = max(max_cnt, cnt)
+                max_Result = max(max_Result, cnt_row, cnt_col)
 
-    print(f"#{test} {max_cnt}")
+
+    print(f"#{testcase} {max_Result}")
