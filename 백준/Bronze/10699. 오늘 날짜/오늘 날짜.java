3d5761943e7509e7ec66686b2d39a96fa4d02a8e
guruguru-tak <kkt3289@gmail.com>
2024-10-31 10:08:55 +0900

    [Bronze V] Title: 오늘 날짜, Time: 156 ms, Memory: 16980 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.time.LocalDate;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		LocalDate N =LocalDate.now();
+		System.out.println(N);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
