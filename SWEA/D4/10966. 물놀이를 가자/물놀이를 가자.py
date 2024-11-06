503906d910d0edce76e1eb3a9ebd2bed0150a799
guruguru-tak <kkt3289@gmail.com>
2024-11-06 13:03:47 +0900

    [D4] Title: 물놀이를 가자, Time: 5,407 ms, Memory: 240,140 KB -BaekjoonHub

--- SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.py
+++ SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.py
@@ -1,29 +1,32 @@
+# import sys
+# sys.stdin = open("input.txt", "r")
 from collections import deque
-#1000 * 1000 => 배열 L 모든 탐색은 10**14 승으로 런타임 에러나게 된다.
+
+# 모든땅 다 체크하면 시간초과
+# 네트워크 퍼지듯 이동값 +1 씩 증가로 저장하고, 모든 grid 내 값 계산하기
 
 t = int(input())
 
 
 def bfs(visited, q, n, m):
-    dx = [1, 0, 0, -1]
-    dy = [0, 1, -1, 0]
+    dx = [0, 1, -1, 0]
+    dy = [1, 0, 0, -1]
 
-    #각 큐 좌표에 4방 탐색
     while q:
-        y, x = q.popleft()
+        cy, cx = q.popleft()
+
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            #벽
-            if 0 > nx or nx >= m or 0 > ny or ny >= n: continue
-            
-            #다음 이동 위치(ny, nx)가 전 위치(y, x)의 값이
+            ny, nx = cy + dy[i], cx + dx[i]
+
+            if ny < 0 or nx < 0 or ny >= n or nx >= m: continue
+
+            # 다음 이동 위치(ny, nx)가 전 위치(cy, cx)의 값이
             # 이동해서 +1인 값보다 크면 (ny, nx)의 값이(ex: 무한대)거나
             # , 큰 값이면 작은 값 갱신
-            if visited[ny][nx] > visited[y][x]+1:
-                visited[ny][nx] = visited[y][x]+1
-                #처음 전체 물 위치 + 앞으로 완탐할 땅 위치 전부 큐에 추가해서
-                #현재 +1해서 2인 위치라면 다음 +1 인 3인 땅을 체크해야 하기에 전부 추가
+            if visited[ny][nx] > visited[cy][cx] + 1:
+                visited[ny][nx] = visited[cy][cx] + 1
+                # 처음 전체 물 위치 + 앞으로 완탐할 땅 위치 전부 큐에 추가해서
+                # 현재 +1해서 2인 위치라면 다음 +1 인 3인 땅을 체크해야 하기에 전부 추가
                 q.append((ny, nx))
 
     # 모든 visited 배열 값 전부 더해서 리턴값 반환
@@ -34,25 +37,29 @@ def bfs(visited, q, n, m):
 
     return move_sum
 
-
-
-
-
 for tc in range(1, t+1):
     n, m = map(int, input().split())
-    grid = [input().strip() for _ in range(n)]
-    visited = [[10**18]*m for _ in range(n)]
-    #결과 담을 변수
-    result = 0
 
-    # 모든 물 위치 큐에 넣어 한 번에 처리하면 시간 초과 안난다
+    grid = [list(map(str, input().strip())) for _ in range(n)]
+
+    # 방문처리
+    visited = [[10**18] * m for _ in range(n)]
+
+    total_Cnt = 0
+
+    # walk = [[10**18] * m for _ in range(n)]
+
+    # 모든 물 좌표 먼저 담아서 보내주기기
     q = deque()
+
     for y in range(n):
         for x in range(m):
-            if grid[y][x] == 'W':
+            # 물일때만 확인으로 시간 줄이기기
+            if grid[y][x] == "W":
                 visited[y][x] = 0
                 q.append((y, x))
 
-    result = bfs(visited, q, n, m)
+    total_Cnt = bfs(visited, q, n, m)
 
-    print(f"#{tc} {result}")
+
+    print(f"#{tc} {total_Cnt}")
