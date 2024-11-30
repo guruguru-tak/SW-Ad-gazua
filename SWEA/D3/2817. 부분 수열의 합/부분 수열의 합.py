5ceac0273cbb0828b117fa506474314533dfc0e4
guruguru-tak <kkt3289@gmail.com>
2024-11-30 19:46:05 +0900

    [D3] Title: 부분 수열의 합, Time: 3,205 ms, Memory: 51,908 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -1,4 +1,5 @@
-from itertools import product, permutations, combinations
+from itertools import combinations
+# 중복을 허용하지 않고 순서를 고려하지 않는 부분집합
 
 t = int(input())
 
@@ -12,12 +13,13 @@ for tc in range(1, t+1):
 
     # 최소 1개 이상의 수를 선택하여 그 합이 K가 되는 경우의 수
     # if sum(i) == k:
-    # 중복허용, 순서 중요 -> 부분 수열
+    # 중복허용 안함, 서로 다른 원소 한번씩 사용 -> 부분 수열
     for index in range(1, n + 1):
         for i in combinations(line, index):
             sum_i = sum(i)
             if sum_i == k:
                 count += 1
+                # print(index)
                 # print(i)
 
             # print(sum_i)
